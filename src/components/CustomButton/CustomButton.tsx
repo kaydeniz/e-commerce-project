28205f938f0a8702afbec719abc9dee09a875be1
kemal <kaydeniz@gmail.com>
2024-03-10 18:19:17 +0300

    CustomButton hover color bug fixed

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
 
 // @ts-ignore
-function CustomButton({ selected, onSelect, label, type, className }) {
-    const buttonClass = selected ? 'bg-1B1B1B' : 'bg-32BE50 hover:bg-32BE50';
-    const labelClass = selected ? 'text-white hover:text-white' : 'text-1B1B1B hover:text-white';
-    const iconClass = selected ? 'text-white hover:text-white' : 'text-1B1B1B hover:text-white';
+function CustomButton({selected, onSelect, label, type, className}) {
+    const buttonClass = selected ? 'bg-1B1B1B hover:text-white' : 'bg-32BE50 hover:bg-32BE50 hover:text-white';
+    const [color, setColor] = React.useState('text-1B1B1B');
+
+    const handleMouseEnter = () => {
+        setColor('text-white')
+    };
+
+    const handleMouseLeave = () => {
+        setColor('text-1B1B1B')
+    };
 
     return (
         <button
-            className={`p-2 rounded-27 ${buttonClass} hover:text-white ${className}`}
+            className={`p-2 rounded-27 ${buttonClass} ${className}`}
             onClick={onSelect}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
             type={type}
         >
             <div className="flex justify-center items-center space-x-2">
-                <span className={labelClass}>{label}</span>
-                <FontAwesomeIcon icon={faArrowRight} className={iconClass} />
+                <span className={color}>{label}</span>
+                <FontAwesomeIcon icon={faArrowRight} className={color}/>
             </div>
         </button>
     );
