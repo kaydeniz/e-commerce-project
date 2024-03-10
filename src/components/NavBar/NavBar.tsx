545059ced18bb33f64eb1b2ada5cd211b1f9827c
kemal <kaydeniz@gmail.com>
2024-03-11 00:12:42 +0300

    NavBar Component updated

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faRightToBracket} from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
     return (
         <nav className="w-full bg-1B1B1B">
-            <div className="flex justify-between items-center px-16 md:px-20 py-4">
-                <div className="flex items-center space-x-4">
-                    <a href="#" className="text-white border-b-2 border-transparent hover:border-white pb-1">For Business</a>
-                    <a href="#" className="text-white border-b-2 border-transparent hover:border-white pb-1">For Drivers</a>
+            <div className="flex justify-between items-center px-16 md:px-20 py-4 relative">
+                <div className="flex items-center space-x-4 h-full">
+                    <div className="relative">
+                        <p className="text-white border-b-2 border-transparent pb-1">For Business</p>
+                        <div className="w-full h-3 bg-FF733C absolute -bottom-6 rounded-t-md"></div>
+                    </div>
+                    <div className="border-l-2 border-545454 h-full mx-2"></div>
+                    <p className="text-white border-b-2 border-transparent hover:border-white pb-1">For Drivers</p>
                 </div>
                 <button className="border-2 border-white text-white flex items-center space-x-2 px-4 py-2">
                     <span>Partner Login</span>
-                    <i className="fas fa-sign-in-alt"></i>
+                    <div className="flex items-centercursor-pointer text-white">
+                        <FontAwesomeIcon icon={faRightToBracket}/>
+                    </div>
                 </button>
             </div>
-            <div className="w-full h-2 bg-1B1B1B rounded-l-md rounded-r-md"></div>
-            <div className="w-full h-2 bg-FF733C rounded-l-md rounded-r-md"></div>
             <div className="w-full bg-FF733C flex justify-between items-center px-16 md:px-20 py-4">
                 <div className="flex items-center space-x-4">
-                    <img src="/BumperLogo.svg" alt="Bumper Logo" />
+                    <img src="/BumperLogo.svg" alt="Bumper Logo"/>
                     <span className="text-black font-oswald font-bold">for business</span>
                 </div>
-                <button className="bg-32BE50 border-2 border-black text-black px-4 py-2 font-open-sans">Register</button>
+                <button className="bg-32BE50 border-2 border-black text-black px-4 py-2 font-open-sans">Register
+                </button>
             </div>
         </nav>
     );
